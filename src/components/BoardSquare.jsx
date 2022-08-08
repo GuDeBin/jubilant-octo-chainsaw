e730ef5f2f9dd1e1daa0c32ae2bd617107954ffb
GuDeBin <gudebin@outlook.com>
2022-08-08 17:54:05 +0800

    Site updated: 2022-08-08 17:54:05

--- src/components/BoardSquare.jsx
+++ src/components/BoardSquare.jsx
@@ -5,7 +5,6 @@ import { useDrop } from "react-dnd";
 
 function BoardSquare({ x, y, children }) {
   const black = (x + y) % 2 === 1;
-  const hello = "hello world";
   return <Square black={black}>{children}</Square>;
 }
 
